ea7b40f797945bdac542bbce16cb06d26ad6ab3a
hdq <haqsir@163.com>
2021-07-27 10:41:45 +0800

    feat: 添加样式文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "src/assets/styles/index.scss";`
+            }
+        }
+    },
+    server: {
+        open: true,
+        proxy: {
+            '/api/v1/chart': {
+                target: 'http://192.168.12.116:8887',
+                changeOrigin: true
+            },
+        }
+    }
 })
+    
