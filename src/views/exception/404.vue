daa2a0ff108bb78af530539cbb2293bcac24c6fd
hdq <haqsir@163.com>
2021-08-15 23:00:33 +0800

    feat: 添加跳转到首页功能

--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -9,10 +9,21 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import { Result } from 'ant-design-vue'
+  import { useRouter } from 'vue-router'
   export default defineComponent({
     name: 'Exception404',
     components: {
       Result
+    },
+    setup() {
+      const router = useRouter()
+
+      const handle = () => {
+        router.push('/dashboard/analysis')
+      }
+      return {
+        handle
+      }
     }
   })
 </script>
--- src/views/exception/500.vue
+++ src/views/exception/500.vue
@@ -1,7 +1,7 @@
 <template>
   <Result status="500" title="500" sub-title="Sorry, the server is reporting an error.">
     <template #extra>
-      <a-button type="primary"> Back Home </a-button>
+      <a-button type="primary" @click="handle"> Back Home </a-button>
     </template>
   </Result>
 </template>
@@ -9,10 +9,21 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import { Result } from 'ant-design-vue'
+  import { useRouter } from 'vue-router'
   export default defineComponent({
     name: 'Exception500',
     components: {
       Result
+    },
+    setup() {
+      const router = useRouter()
+
+      const handle = () => {
+        router.push('/dashboard/analysis')
+      }
+      return {
+        handle
+      }
     }
   })
 </script>
