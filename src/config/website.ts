8af625736035d2fa441de3ba23389e00a3e584bf
hdq <haqsir@163.com>
2021-09-06 16:47:26 +0800

    feat: 添加本地存储localstorage, sessstorage

---
+++ src/config/website.ts
@@ -0,0 +1,3 @@
+
+export const storageKey = 'viteAdmin';//配置主键,目前用于存储
+export const iconScriptUrl = '//at.alicdn.com/t/font_2769259_zfx85i4mvph.js';
---
+++ src/utils/storage.ts
@@ -0,0 +1,74 @@
+import { storageKey } from '/@/config/website'
+
+const useStorage = ($storage) => {
+    /**
+     * 根据 key 值获取储存在 storage 中的值
+     * @param key storage key
+     */
+    const get = (key: string) => {
+        let value = $storage.getItem(`${storageKey}-${key}`);
+        try {
+            value = JSON.parse(value);
+            return value;
+        } catch {
+            return value;
+        }
+    }
+
+    /**
+     * 根据 key 值向 storage 中储存值
+     * @param key storage key
+     * @param value 需要储存在 storage 中的值
+     */
+    const set = (key: string, value: any) => {
+        return $storage.setItem(`${storageKey}-${key}`, value ? JSON.stringify(value) : value);
+    }
+
+    /**
+     * 根据 key 值移除储存在 storage 中的值
+     * @param key storage key
+     */
+    const remove = (key: string) => {
+        return $storage.removeItem(`${storageKey}-${key}`);
+    }
+
+    /**
+     * 移除除了 key 之外的所有储存在 storage 中的值
+     * @param key storage key
+     */
+    const clearExcept = (key: string) => {
+        for (let i = 0; i < $storage.length; i++) {
+            const itemKey: string | undefined = $storage.key(i);
+            if (itemKey && itemKey !== `${storageKey}-${key}`) {
+                $storage.removeItem(itemKey);
+            }
+        }
+    }
+
+    /**
+     * 移除所有储存在 storage 中的值
+     */
+    const clearAll = () => {
+        for (const itemKey in $storage) {
+            if (itemKey) {
+                $storage.removeItem(itemKey);
+            }
+        }
+    }
+
+    return {
+        get,
+        set,
+        remove,
+        clearExcept,
+        clearAll,
+    }
+}
+ 
+const sessionStorageService = useStorage(window.sessionStorage || sessionStorage)
+const localStorageService = useStorage(window.localStorage || localStorage)
+
+export {
+    sessionStorageService,
+    localStorageService,
+}
