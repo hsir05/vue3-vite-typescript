79654f5d30fd3f71adbe1834837540cbb25c5f51
hsir05 <haqsir@163.com>
2021-10-09 17:25:37 +0800

    feat: 菜单分割功能

--- src/layouts/LayoutSider.vue
+++ src/layouts/LayoutSider.vue
@@ -1,16 +1,17 @@
 <template>
-  <Sider v-model:collapsed="getCollapsed" class="h-sider-bg">
+  <Sider v-model:collapsed="getCollapsed" :class="['h-sider-bg', active ? 'h-sider-active' : '']">
     <AppLogo v-if="getLayoutMode === 'sidebar'" />
     <HMenu :isHorizontal="false" />
   </Sider>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, ref } from 'vue'
   import { Layout } from 'ant-design-vue'
   import AppLogo from './AppLogo.vue'
   import HMenu from './HMenu.vue'
   import { useMenuSetting } from '/@/hooks/setting/useMenuSetting'
   import { useHeaderSetting } from '/@/hooks/setting/useHeaderSetting'
+  import { subMenuEmitter } from '/@/layouts/menuChange'
 
   export default defineComponent({
     name: 'LayoutSider',
@@ -23,9 +24,20 @@
       const { getCollapsed } = useMenuSetting()
       const { getLayoutMode } = useHeaderSetting()
 
+      // 函数待优化
+      const active = ref(false)
+      if (getLayoutMode.value === 'mix-sidebar') {
+        active.value = true
+      }
+
+      subMenuEmitter.on('listenMenuData', (childrenMenuData) => {
+        active.value = childrenMenuData.length > 0 ? false : true
+      })
+
       return {
         getCollapsed,
-        getLayoutMode
+        getLayoutMode,
+        active
       }
     }
   })
@@ -34,17 +46,20 @@
   .h-sider-bg.ant-layout-sider {
     background-color: var(--sider-dark-bg-color);
   }
+  .h-sider-active.ant-layout-sider {
+    flex: 0 0 0 !important;
+    max-width: 200px;
+    min-width: 0 !important;
+  }
   .ant-menu-vertical .ant-menu-item:not(:last-child) {
     margin-top: 0;
   }
 
   .h-sider-bg {
     .ant-menu.ant-menu-dark {
-      // background-color: var(--sider-dark-darken-bg-color);
       background-color: var(--sider-dark-bg-color);
     }
     .ant-menu-dark .ant-menu-inline.ant-menu-sub {
-      //  background-color: var(--sider-dark-bg-color);
       background-color: var(--sider-dark-darken-bg-color);
     }
   }
